5b4ec5dd12bba32bd07ff691009f72e9484dc1c6
Tocsin614 <paul.borrows@yahoo.com>
2017-05-30 23:02:56 -0400

    v1.5 Weedle to the rescue

--- Riven - Tocsin.lua
+++ Riven - Tocsin.lua
@@ -1,11 +1,10 @@
 class "Riven"
- --------- Yosh E R Hydra W Q R Q Q Ignite Dead 
+ 
 require = 'DamageLib'
 
 function Riven:__init()
 	if myHero.charName ~= "Riven" then return end
 PrintChat("Riven - Tocsin loaded")
-PrintChat("YOU MUST KEYBIND DANCE TO Y FOR THE CANCELLING TO WORK")
 self:LoadSpells()
 self:LoadMenu()
 Callback.Add("Tick", function() self:Tick() end)
@@ -43,7 +42,7 @@ function Riven:LoadMenu()
 	self.Menu.Combo:MenuElement({id = "UseE", name = "Use E", value = true, leftIcon = Icons.E})
 	self.Menu.Combo:MenuElement({id = "UseR", name = "Use R", value = true, leftIcon = Icons.R})
 	self.Menu.Combo:MenuElement({id = "ER", name = "Min enemies to use R", value = 1, min = 1, max = 5})
-	self.Menu.Combo:MenuElement({id = "Exhaust", name = "Use Exhaust", value = true, leftIcon = Icons.EXH})
+	self.Menu.Combo:MenuElement({id = "Exhaust", name = "Use Exhaust", value = false, leftIcon = Icons.EXH})
 --Harass Settings Menu
 
 	self.Menu:MenuElement({type = MENU, id = "Harass", name = "Harass Settings"})
@@ -51,23 +50,26 @@ function Riven:LoadMenu()
 	self.Menu.Harass:MenuElement({id = "UseW", name = "Use W", value = true, leftIcon = Icons.W})
 	self.Menu.Harass:MenuElement({id = "UseE", name = "Use E", value = true, leftIcon = Icons.E})
 
---misc
-	self.Menu:MenuElement({type = MENU, id = "Misc", name = "Misc"})
-		if myHero:GetSpellData(4).name == "SummonerDot" or myHero:GetSpellData(5).name == "SummonerDot" then
-			self.Menu.Misc:MenuElement({id = "UseIgnite", name = "Use Ignite", value = true})
-		end
+--[[LastHit 
 
---LastHit
-
-	--self.Menu:MenuElement({type = MENU, id = "LastHit", name = "Last Hit"})
-	--self.Menu.LastHit:MenuElement({id = "UseQ", name = "Use Q", value = true, leftIcon = Icons.Q})
-
+	self.Menu:MenuElement({type = MENU, id = "LastHit", name = "Last Hit"})
+	self.Menu.LastHit:MenuElement({id = "UseQ", name = "Use Q", value = true, leftIcon = Icons.Q})
+	self.Menu.LastHit:MenuElement({id = "UseE", name = "Use E", value = true, leftIcon = Icons.E})
+	
+--]]
 --JungleClear
 	self.Menu:MenuElement({type = MENU, id = "JungleClear", name = "Jungle Clear"})
   	self.Menu.JungleClear:MenuElement({id = "Q", name = "Use Q", value = true, leftIcon = Icons.Q})
   	self.Menu.JungleClear:MenuElement({id = "W", name = "Use W", value = true, leftIcon = Icons.W})
   	self.Menu.JungleClear:MenuElement({id = "E", name = "Use E", value = true, leftIcon = Icons.E})
  
+
+--misc
+	self.Menu:MenuElement({type = MENU, id = "Misc", name = "Misc"})
+		if myHero:GetSpellData(4).name == "SummonerDot" or myHero:GetSpellData(5).name == "SummonerDot" then
+			self.Menu.Misc:MenuElement({id = "UseIgnite", name = "Use Ignite", value = true})
+		end
+
 --Drawings Settings Menu
 
 	self.Menu:MenuElement({type = MENU, id = "Drawings", name = "Drawing Settings"})
@@ -84,123 +86,95 @@ function Riven:Tick()
 		elseif _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_JUNGLECLEAR] then
 			self:JungleClear()
 		end
-		self:Misc()
+	self:Misc()
 end
 
 function Riven:Draw()
 	if myHero.dead then return end
 	if(self.Menu.Drawings.drawW:Value())then
-		Draw.Circle(myHero, 260, 3, Draw.Color(225, 225, 0, 10))
+		Draw.Circle(myHero, W.range, 3, Draw.Color(255, 225, 255, 10))
 	end
 	if(self.Menu.Drawings.drawQ:Value())then
-		Draw.Circle(myHero, 380, 3, Draw.Color(255, 255, 255, 10))
+		Draw.Circle(myHero, 440, 3, Draw.Color(225, 225, 0, 10))
 	end
 end
-
+--Combo aka spacebar
 function Riven:Combo()
-	local ytarg = _G.SDK.TargetSelector:GetTarget(700)
-	local ItemHotKey = {
-    [ITEM_1] = HK_ITEM_1,
-    [ITEM_2] = HK_ITEM_2,
-    [ITEM_3] = HK_ITEM_3,
-    [ITEM_4] = HK_ITEM_4,
-    [ITEM_5] = HK_ITEM_5,
-    [ITEM_6] = HK_ITEM_6,}
-	if GetItemSlot(myHero, 3142) >= 1 and self:CanCast(_Q) and ytarg then 
-		if self:IsReady(GetItemSlot(myHero, 3142)) then
-			Control.CastSpell(ItemHotKey[GetItemSlot(myHero, 3142)], self)
-		end 
-	end
-	local etarg = _G.SDK.TargetSelector:GetTarget(620)
-		if etarg and self.Menu.Combo.UseE:Value() and self:CanCast(_E) then
-			self:CastE(etarg)
-		end
-
-	local wtarg = _G.SDK.TargetSelector:GetTarget(260)
-		if wtarg and self.Menu.Combo.UseW:Value() and self:CanCast(_W) then
-			Control.CastSpell(HK_W)
-		end
-
-	local  rtarg = _G.SDK.TargetSelector:GetTarget(R.range) 
-		if rtarg and self.Menu.Combo.UseR:Value() and self:CanCast(_R) and not self:HasBuff(myHero, "rivenwindslashready") then
-    		if self:CountEnemys(260) >= self.Menu.Combo.ER:Value() then
-    			self:CastR(rtarg)
-      		end
-    	end
-
-    local htarg = _G.SDK.TargetSelector:GetTarget(350)
-	local ItemHotKey = {
-    [ITEM_1] = HK_ITEM_1,
-    [ITEM_2] = HK_ITEM_2,
-    [ITEM_3] = HK_ITEM_3,
-    [ITEM_4] = HK_ITEM_4,
-    [ITEM_5] = HK_ITEM_5,
-    [ITEM_6] = HK_ITEM_6,}
-	if GetItemSlot(myHero, 3074) >= 1 and myHero.attackData.state == STATE_WINDDOWN and not self:CanCast(_E) then 
-		if self:IsReady(GetItemSlot(myHero, 3074)) then
-			Control.CastSpell(ItemHotKey[GetItemSlot(myHero, 3074)], self)
-		end 
-	end
-
-	local starg = _G.SDK.TargetSelector:GetTarget(350)
-	local ItemHotKey = {
-    [ITEM_1] = HK_ITEM_1,
-    [ITEM_2] = HK_ITEM_2,
-    [ITEM_3] = HK_ITEM_3,
-    [ITEM_4] = HK_ITEM_4,
-    [ITEM_5] = HK_ITEM_5,
-    [ITEM_6] = HK_ITEM_6,}
-	if GetItemSlot(myHero, 3077) >= 1 and self:CanCast(_Q) and starg then 
-		if self:IsReady(GetItemSlot(myHero, 3077)) and myHero.attackData.state == STATE_WINDDOWN and not self:CanCast(_E) then
-			Control.CastSpell(ItemHotKey[GetItemSlot(myHero, 3077)], self)
-		end 
-	end
-
-	local qtarg = _G.SDK.TargetSelector:GetTarget(440)
-		if qtarg and self.Menu.Combo.UseQ:Value() and self:CanCast(_Q) and myHero.attackData.state == STATE_WINDDOWN then
-			local castPosition = qtarg
-			self:CastQ(castPosition)
-			DelayAction(function()
-			Control.CastSpell(string.byte("Y")) -- user change dance to Y
-			end, 0.15)
-		end
+	if _G.SDK.TargetSelector:GetTarget(700) == nil then return end
 	
-	local  ztarg = _G.SDK.TargetSelector:GetTarget(900) 
-		if ztarg and self.Menu.Combo.UseR:Value() and self:CanCast(_R) and self:HasBuff(myHero, "rivenwindslashready") then
-    		self:Wings(ztarg)
-    	end	
+	if self.Menu.Combo.UseE:Value() and self:CanCast(_E) then
+		self:CastE(etarg)
+	end
 
-    local xtarg = _G.SDK.TargetSelector:GetTarget(600)
-		if xtarg and self.Menu.Combo.Exhaust:Value() and not self:CanCast(_E) and not self:CanCast(_Q) then
-			self:Exhaust(xtarg)
-		end
+	if self.Menu.Combo.UseW:Value() and self:CanCast(_W) then
+		self:CastW(wtarg)
+	end
 
+	if self.Menu.Combo.UseQ:Value() and self:CanCast(_Q) then
+		self:CastQ(qtarg)
+	end
+	
+	if self.Menu.Combo.UseR:Value() and self:CanCast(_R) then
+    	self:CastR(rtarg)
+    end
+ 	if self.Menu.Combo.UseR:Value() and self:CanCast(_R) then
+    	self:Wings(ztarg)
+    end
+    if self.Menu.Combo.Exhaust:Value() and not self:CanCast(_E) and not self:CanCast(_Q) then
+		self:Exhaust(xtarg)
+	end
 end
 
-function Riven:Harass()
-	local etarg = _G.SDK.TargetSelector:GetTarget(620)
-		if etarg and self.Menu.Combo.UseE:Value() and self:CanCast(_E) then
-			self:CastE(etarg)
-		end
+function Riven:Harass() 
+	if _G.SDK.TargetSelector:GetTarget(700) == nil then return end
+	
+	if self.Menu.Combo.UseE:Value() and self:CanCast(_E) then
+		self:CastE(etarg)
+	end
 
-	local wtarg = _G.SDK.TargetSelector:GetTarget(260)
-		if wtarg and self.Menu.Combo.UseW:Value() and self:CanCast(_W) then
-			Control.CastSpell(HK_W)
-		end
+	if self.Menu.Combo.UseW:Value() and self:CanCast(_W) then
+		self:CastW(wtarg)
+	end
 
-	local qtarg = _G.SDK.TargetSelector:GetTarget(440)
-		if qtarg and self.Menu.Combo.UseQ:Value() and self:CanCast(_Q) and not self:CanCast(_W) and myHero.attackData.state == STATE_WINDDOWN then
-			local castPosition = qtarg
-			self:CastQ(castPosition)
-			DelayAction(function()
-			Control.CastSpell(string.byte("Y")) -- user change dance to Y
-			end, 0.15)
-		end
+	if self.Menu.Combo.UseQ:Value() and self:CanCast(_Q) then
+		self:CastQ(qtarg)
+	end
+		
+end
 
+function Riven:JungleClear()
+  	if self:GetValidMinion(620) == false then return end
+  	for i = 1, Game.MinionCount() do
+	local minion = Game.Minion(i)
+    	if  minion.team == 300 or 200 then
+			if self:IsValidTarget(minion,620) and myHero.pos:DistanceTo(minion.pos) < 620 and self.Menu.JungleClear.E:Value() and self:CanCast(_E) then
+				Control.CastSpell(HK_E,minion.pos)
+			break
+			end    		
+			if self:IsValidTarget(minion,260) and myHero.pos:DistanceTo(minion.pos) < 260 and self.Menu.JungleClear.W:Value() and self:CanCast(_W) then
+				Control.CastSpell(HK_W)
+			break
+			end
+			if self:IsValidTarget(minion,440) and myHero.pos:DistanceTo(minion.pos) < 440 and self.Menu.JungleClear.Q:Value() and self:CanCast(_Q) and myHero.attackData.state == STATE_WINDDOWN then
+				Control.CastSpell(HK_Q,minion.pos)
+			break
+			end
+		end
+	end
 end
 
 function Riven:IsValidTarget(unit,range) 
-	return unit ~= nil and unit.valid and unit.visible and not unit.dead and unit.isTargetable and not unit.isImmortal and unit.pos:DistanceTo(myHero.pos) <= 875 
+	return unit ~= nil and unit.valid and unit.visible and not unit.dead and unit.isTargetable and not unit.isImmortal and unit.pos:DistanceTo(myHero.pos) <= 620 
+end
+
+function Riven:GetValidMinion(range)
+    	for i = 1,Game.MinionCount() do
+        local minion = Game.Minion(i)
+        if  minion.team ~= myHero.team and minion.valid and minion.pos:DistanceTo(myHero.pos) < 620 then
+        return true
+        end
+    	end
+    	return false
 end
 
 function Riven:HpPred(unit, delay)
@@ -212,21 +186,24 @@ function Riven:HpPred(unit, delay)
 	return hp
 end
 
+
 function Riven:Wings(target)
-	if target then
-		if not target.dead and not target.isImmune then
-			if target.distance<=850 and target.health/target.maxHealth <= 0.23 then
-				local pred=target:GetPrediction(R.speed,R.delay)
-				Control.CastSpell(HK_R,pred)
+	local ztarg = _G.SDK.TargetSelector:GetTarget(850)
+	if ztarg and self.Menu.Combo.UseR:Value() and self:CanCast(_R) then
+			if self:HasBuff(myHero, "rivenwindslashready") then
+				if ztarg.distance<=850 and ztarg.health/ztarg.maxHealth <= 0.23 then
+					local pred=ztarg:GetPrediction(R.speed,R.delay)
+					Control.CastSpell(HK_R,pred)
+				end
 			end
-		end
 	end
-return false
+
 end
 
-	
 function Riven:Exhaust(target)
-	if target then 
+	local xtarg = _G.SDK.TargetSelector:GetTarget(350)
+	if self.Menu.Combo.Exhaust:Value() and not self:CanCast(_E) and not self:CanCast(_Q) then
+	if xtarg then 
    		if myHero:GetSpellData(SUMMONER_1).name == "SummonerExhaust" and self:IsReady(SUMMONER_1) then
        		if self:IsValidTarget(target, 600, true, myHero) and target.health/target.maxHealth <= 0.4 then
             	Control.CastSpell(HK_SUMMONER_1, target)
@@ -238,49 +215,15 @@ function Riven:Exhaust(target)
 		end
 	end
 end
-
---KS on tick
-function Riven:Misc()
-     for i = 1,Game.HeroCount() do
-        local Enemy = Game.Hero(i)
-            if myHero:GetSpellData(5).name == "SummonerDot" and self.Menu.Misc.UseIgnite:Value() and self:IsReady(SUMMONER_2) then
-                if self:IsValidTarget(Enemy, 600, false, myHero.pos) and Enemy.health + Enemy.hpRegen*2.5 + Enemy.shieldAD < 50 + 20*myHero.levelData.lvl then
-                    Control.CastSpell(HK_SUMMONER_2, Enemy)
-                    return;
-                end
-            end
-
-            if myHero:GetSpellData(4).name == "SummonerDot" and self.Menu.Misc.UseIgnite:Value() and self:IsReady(SUMMONER_1) then
-                if self:IsValidTarget(Enemy, 600, false, myHero.pos) and Enemy.health + Enemy.hpRegen*2.5 + Enemy.shieldAD < 50 + 20*myHero.levelData.lvl then
-                    Control.CastSpell(HK_SUMMONER_1, Enemy)
-                    return;
-                end
-            end
-        end
 end
 
-function Riven:JungleClear()
-  	if self:GetValidMinion(Q.range) == false then return end
-  	for i = 1, Game.MinionCount() do
-	local minion = Game.Minion(i)
-		if  minion.team == 300 or 200 then
-			if self:IsValidTarget(minion,340) and myHero.pos:DistanceTo(minion.pos) < 240 and self.Menu.JungleClear.Q:Value() and self:CanCast(_Q) and myHero.attackData.state == STATE_WINDDOWN then
-			Control.CastSpell(HK_Q,minion.pos)
-			DelayAction(function()
-			Control.CastSpell(string.byte("Y")) -- user change dance to Y
-			end, 0.15)
-			break
-			end
-			if self:IsValidTarget(minion,120) and myHero.pos:DistanceTo(minion.pos) < 120 and self.Menu.JungleClear.W:Value() and self:CanCast(_W) and myHero.attackData.state == STATE_WINDDOWN then
-				Control.CastSpell(HK_W)
-			break
-			end
-			if self:IsValidTarget(minion,320) and myHero.pos:DistanceTo(minion.pos) < 300 and self.Menu.JungleClear.E:Value() and self:CanCast(_E) and myHero.attackData.state == STATE_WINDDOWN then
-			Control.CastSpell(HK_E,minion.pos)
-			break
-			end
-		end
-	end
+local function GetItemSlot(unit, id)
+  for i = ITEM_1, ITEM_7 do
+    if unit:GetItemData(i).itemID == id then
+      return i
+    end
+  end
+  return 0 
 end
 
 function Riven:HasBuff(unit, buffname)
@@ -293,76 +236,106 @@ end
 return false
 end
 
-function GetItemSlot(unit, id)
-  for i = ITEM_1, ITEM_7 do
-    if unit:GetItemData(i).itemID == id then
-      return i
-    end
-  end
-  return 0 
-end
-
-function Riven:GetValidMinion(range)
-    	for i = 1,Game.MinionCount() do
-        local minion = Game.Minion(i)
-        if  minion.team ~= myHero.team and minion.valid and minion.pos:DistanceTo(myHero.pos) < Q.range then
-        return true
-        end
-    	end
-    	return false
-end
-
 function Riven:CountEnemys(range)
 	local heroesCount = 0
     	for i = 1,Game.HeroCount() do
         local enemy = Game.Hero(i)
-        if  enemy.team ~= myHero.team and enemy.valid and enemy.pos:DistanceTo(myHero.pos) < 250 then
+        if  enemy.team ~= myHero.team and enemy.valid and enemy.pos:DistanceTo(myHero.pos) < 260 then
 			heroesCount = heroesCount + 1
         end
     	end
 		return heroesCount
 end 
 
+LastCancel = Game.Timer()
+function Riven:CastQ(target)
+    local qrange = 440 --myHero:GetSpellData(_Q).range
+    local qtarg = _G.SDK.TargetSelector:GetTarget(qrange)
+    if qtarg then
+        if qtarg.dead or qtarg.isImmune then return end
+        if myHero.pos:DistanceTo(qtarg.pos) < myHero.range then
+            if self.Menu.Combo.UseQ:Value() and self:CanCast(_Q) and myHero.attackData.state == STATE_WINDDOWN then
+                Control.CastSpell(HK_Q,qtarg)
+                if Game.Timer() - LastCancel > 0.15 then
+                LastCancel = Game.Timer()
+                    DelayAction(function()
+                    local Vec = Vector(myHero.pos):Normalized() * - (myHero.boundingRadius*1.1)
+                    Control.Move(Vec)
+                    end, 0.15)
+                end
+            end
+        else
+            if self.Menu.Combo.UseQ:Value() and self:CanCast(_Q) then
+                Control.CastSpell(HK_Q,qtarg) 
+                if Game.Timer() - LastCancel > 0.15 then
+                LastCancel = Game.Timer()
+                    DelayAction(function()
+                    local Vec = Vector(myHero.pos):Normalized() * - (myHero.boundingRadius*1.1)
+                    Control.Move(Vec)
+                    end, 0.15)
+                end
+            end
+        end
+    end
+end
+
+function Riven:CastW(target) 
+	local wtarg = _G.SDK.TargetSelector:GetTarget(260)
+	if wtarg and self.Menu.Combo.UseW:Value() and self:CanCast(_W) then
+		Control.CastSpell(HK_W)
+	end
+end
+				
+
 function Riven:CastE(target)
-	if target then
-		if not target.dead and not target.isImmune then
-			if target.distance<=620 then
-				local pred=target:GetPrediction(E.speed,E.delay)
-				Control.CastSpell(HK_E,pred)
-			end
-		end
+	local etarg = _G.SDK.TargetSelector:GetTarget(620)
+	if etarg and self.Menu.Combo.UseE:Value() and self:CanCast(_E) then
+		local pred=etarg:GetPrediction(E.speed,E.delay)
+		Control.CastSpell(HK_E,pred)
 	end
-return false
 end
 
-function Riven:CastW(target)
-	if target then
-		if not target.dead and not target.isImmune then
-			if target.distance<=260 then
-				Control.CastSpell(HK_W)
-			end
-		end
-	end
-return false
-end
-
-function Riven:CastQ(position)
-	if position then
-		Control.SetCursorPos(position)
-		Control.CastSpell(HK_Q, position)
-	end
-end
 
 function Riven:CastR(target)
-	if target then
-		if not target.dead and not target.isImmune then
-			if target.distance<=R.range then
-				local pred=target:GetPrediction(R.speed,R.delay)
-				Control.CastSpell(HK_R,pred)
-			end
-		end
+    local rtarg = _G.SDK.TargetSelector:GetTarget(620)
+    if rtarg and self.Menu.Combo.UseR:Value() and self:CanCast(_R) and not self:HasBuff(myHero, "rivenwindslashready") then
+    	if self:CountEnemys(260) >= self.Menu.Combo.ER:Value() then
+    		Control.CastSpell(HK_R)
+      	end
+    end
+end
+
+--KS
+function Riven:Misc()
+	if _G.SDK.TargetSelector:GetTarget(650) == nil then return end
+     	for i = 1,Game.HeroCount() do
+        local Enemy = Game.Hero(i)
+            if myHero:GetSpellData(5).name == "SummonerDot" and self.Menu.Misc.UseIgnite:Value() and self:IsReady(SUMMONER_2) then
+                if self:IsValidTarget(Enemy, 600, false, myHero.pos) and Enemy.health + Enemy.hpRegen*2.5 + Enemy.shieldAD < 50 + 20*myHero.levelData.lvl then
+                    Control.CastSpell(HK_SUMMONER_2, Enemy)
+                    return;
+                end
+            end
+            if myHero:GetSpellData(4).name == "SummonerDot" and self.Menu.Misc.UseIgnite:Value() and self:IsReady(SUMMONER_1) then
+                if self:IsValidTarget(Enemy, 600, false, myHero.pos) and Enemy.health + Enemy.hpRegen*2.5 + Enemy.shieldAD < 50 + 20*myHero.levelData.lvl then
+                    Control.CastSpell(HK_SUMMONER_1, Enemy)
+                    return;
+                end
+            end
+        end
+end
+
+function Riven:GetAllyHeroes()
+	local _AllyHeroes
+	if _AllyHeroes then return _AllyHeroes end
+	_AllyHeroes = {}
+	for i = 1, Game.HeroCount() do
+    local unit = Game.Hero(i)
+    if unit.isAlly then
+      table.insert(_AllyHeroes, unit)
+    end
 	end
-return false
+	return _AllyHeroes
 end
 
 function Riven:IsReady(spellSlot)
