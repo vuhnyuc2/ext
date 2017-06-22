10dc70c9f259ec296b03371b0734a0407c4adda8
Tocsin614 <paul.borrows@yahoo.com>
2017-06-22 18:55:51 -0400

    v1.0

--- Malzahar_Tocsin.lua
+++ Malzahar_Tocsin.lua
@@ -1,3 +1,88 @@
+--                                                                                                                                                                                                                                                                                                         
+--                                                                                                                                                      
+--                                                 :;.         .                                               ,,                                       
+--                                               ;i;          .     ..::,,;;;::.,                              ,2r                                      
+--                                              Fr   .    ;r      .,;;::..   .   ...        ;:                   ;2,                                    
+--                                            .br       .r.r    .,rr;,......;,,,,. .:       irr                   :P;                                   
+--                                            C;         i ;:  ,rL:             .:;:..     r; c                    :;;;                                 
+--                                           cr   . .r   5, r:rjc.                 :rr: . .7 ::   r                ,; ;r,                               
+--                                          ;C     ;:r5  r, :Lr,                ;:   :ir:.;  t   j;r:              .r   r;                              
+--                                          n      ;  Z .;.r7.                 ;;..    .rc. r;  jr ;i               ,    ,r.                            
+--                                         Fr      r, ;i;;r,                  ,r  ;.     ;r:;; ir  5                      .c:                           
+--                                        .X       ,i  ;;:            ,, .   .c   :;       :r;i;  i;                        L;                          
+--                                        j,       .; ;r           ..,,.;;  ,i     :   ;r   .r,  :r                          C.                         
+--                                       X@       ;:,r,           , :;: :  ,r.     ; : ,r.,   ;; c,                           T                         
+--                                      F@;      r;;;    ;  ,.  ,; ,;. ,..;r      .,;s ;,  ;.  ,iri.                 . ,      ;r                        
+--                                    .tXL     .rr;:    ;U:;; .,, ;:  ,;cr,       .r:; ;,   r    7;r;                  :,      r;                       
+--                                   :c..     ,;;, r  .r5L ; ..  ;. .r;r;         i;,..r    ,;    rcr;                  ;       r                       
+--                                  :t      ::;r  .;  i;d  :,:  .,;r;.           Lr ;:;      ,c r  ;;,r                 :r      :j  .                   
+--                                  F,     ;;7r  ;r  r.L. :::  :;;,             r;  i;. . .:.iT;;;  ;r.r:                j:      t;                     
+--                                 ;c     ;;rr. :;; r;ir ;T                    :.  ;i...;,:..5;;;r,  .r,;;                U       O                     
+--                                 n    .;;U,; ; ..;;.:.:F;.                      ;n,:,,,,...:  r;.r   r;,i,              ST      rr                    
+--                                rT   ;;r;;c;;  ;;.    ;, :;:.                  .Cr:..         s. r:   rr.5;             .Z      .U                    
+--                                F  .rrc..rx;  rr           .:r;               ;r.    ..;;; .  ;.  ; .  .;,;;             g:      r:                   
+--                               rs :;,i;  Cx :r;      ...,,.   :r7   ;  ;    :r;  .;;rxcc7rs;;.    .,r;   C;.L            rx       r                   
+--                               O ;j;: ;, sx:i.   ,rr;:rc::::;;  ;j;r:  ;r,;;. .rrr;.;.    ,; ;U:   ;jr  .r,;.r           .s       j:                  
+--                              ;r;L;:  .c .L   :r:.    i      2::;.;.    ;i, ;;:.    r      ;;T:    ;i;  i. r; r,          r       rT                  
+--                              CiLr:    t  s   .cCr;    ;    ,n;;tr..    .. Cx;,:;;:.,rrr,cU2L.     ;;;  : , 7i i          T.      .i                  
+--                             rO;i;     i  s     .;HFirrEgsT7Lr;.  ,       : .;;;rrrr;iCrrSc          ; ;  ; ,7T r        .P        i                  
+--                             Lrrr      s. c        : :ir          .;     ..           : r           .,r; ;  ; Cr ;       ;H  .::,: s.                 
+--                            ,xrr       ;r r        : .:                                .;           ::S, ; .;  g..;      r;.r;:..:rT                  
+--                            cL,;        :TT        ;.;         .,;;;rr;r,:..           :;.         .i.;  ,;r:  ;H r.     d;:      :L                  
+--                            iX ;         ,c        ;;,      ,;r;;;;;,.:.:;;:;;,       ,,;.        .r. r ;;7S   rc 5     xg                            
+--                             #:L          r.       ;;.    ,irr;;,. .   ...,;;;;;;.    ::,            r;.;.F;   X x;    ,P        .                    
+--                             r:s;         x; .     :.,.,;cr:,,.             ..,;r;;:. ;.;           ,r ;:.r   O rc    .O.                             
+--                              s i         ;c,;;;;;.c;r7r;;,.                    .,;;:r:7,       ..;;.r.c     S;;n     8;                              
+--                              :r r         ;..;:r;:,;,:..                         .,;;;;;:;;;::;r;:.Tjr.    rr;;    .O;                               
+--                               ;i.r.       .; r ,:                                     ...:r:.r:,: ,:Ts    rirr    r6.                                
+--                                ;r:;:       r::. r                                         r  ;    r r.   ;ir;    LF:;                                
+--                                  ;;;r       j ; ;                                        ;, ;.   r,     i7r,  ..F6, ,r  .                            
+--                                   ,;;s;     .;;,.:                                   .  .r ,;   ;r     s;r  ;7;,.    ;:                              
+--                                     ;:rs.    ;r;.;                                     .; ;;   .T     r,;:;r;         i                              
+--                                      rr:rr    ;;;T;                                   .r ;r    r    ;; 7rr:          .r                              
+--                             .    .;rc;.;r;;    ;  .                                  ;L;;,    i,   r..Pc:            .s     .                        
+--                                .Tt;:    ;Lrr:   ;                                    ..      ri  ;; U@;               x;:;.                          
+--                               ,c;  .;     r7rr  .r                                          ;;; s,:BD                 r; ,;;                         
+--                              .L.    :r.    .;7r.  ;                                        r:.j; ;D;                  .2;. :i                        
+--                             .T.      .;;     .rs; .d                                      ;r;: .r;                   ;Li.   ;c                       
+--                           :rrr         ;r;     ;si.C;.                                   Zj:.:ti                  .ri;       r;                      
+--                          7j;r            ,;;.   .7nr ;.                                :tr:;7;.               .;ci;.          ;;.                    
+--                        .r; ,r  .           ,;;.   ;r: ;:                             ;ri;;;.               .;rrr,          .   .r:                   
+--                   .   ,L.   ,;               .rT;  ,L: ,r                        .76nc;,.               :rcr;.                   r.                  
+--                       s.     .rr               ,;si,;6; .i.               .,,;;;;;r:           .     :;rr;                       rC                  
+--                     :B;       .cS;                .;,rbj. ;;.     ..,:::r;:,:..                 .:rrr;,                          5Cr                 
+--                    ;Sx;          rr7:                  rrr;SZrrr;:::.. .                   .:;;;;;,                      .      T: rT.               
+--                .,,.X  U:           :cTr,                                              .,;rr;;.                                 rr   .;;              
+--           .;;,.... c   s;             :;rr:,                                     .,::;;:,.                                   ;i;      .r             
+--            .;rs;   c.   ;;                :rr;;.,.                      ..::;;rrr;,.                                      .;r;         x             
+--           ..,;r.   .i    ,;.                 ..,,:;r;;;;;;:;,,,,,;:;;;r7;::;,.                                         ,:rr;          Ui:,           
+--        .:r::r.      :i    .;r                         ..;;;;;;;;;....                                              .;rc;,           :i;  ;:          
+--      ;r:   ;,:r,     .;;    :r;    .                                                                     .      .;;;:.             ss     ,;   .     
+--    rr.      .:         rC:    rc;   .                                                                      .;;;:,.              :;i:        :.       
+--  ;r;   .  ,;;           .xF:   .;;,                                                                  .:;;;;;:.               ,;ir,        .;.;:      
+-- ;c.      ,; :;            :Tr     ,:,.            ,:,,, .                                      .,:;;;;;,.               .,;;7r;     .;;,;,.s7;r      
+--r:           ;.         .     ,;.     ,::.            ..,,,,,,:.. . .                     ..;;r;:,.                 .rTijr;,.        ;; . ... :;r     
+--.  .       .,.                  ,;:     .;,.                 ..,,,,:;;;r;;::::,:,:,:::,:::,:..                   ,:rcr,              .;        Cc     
+--         .;;;                     :;;.                                      . .........           .,.         ,;rr;           .:,,,;; ;,       :t     
+--           ::                       :r;.                                                     ..:;;:,.      :;;;,               r       .        r:    
+--          ,,                          :rr.                                                ,,;r:.        ,;r;.             ,.r:.;;               ;5    
+--         :                               ;,                                             :;.            ,.                 , .                    n    
+--
+--   r2X99H9H9HGGi  sX99H9H9H993;                        ,,                                          ,:                                                 
+-- ,B@B@B@B@B@@@B@B@B@@@B@B@B@B@B@,                     B@Br                                        ;@B@                                                
+-- B@Bs          B@@B          s@B@                     @B@r                                        ;B@@                                                
+-- @B@           s@@5           B@B                     B@Br                                        ;@B@                                                
+-- B@B,          3@BA          ,@B@   ,GGAAAAAA2A23;    @@@;     :9XXAXAA22sss32r  s9AAAA2A2AA2i,   ;@@Bi2XAXAA2s;    s9XAA2AAAAA2s,      i3AAAAX22r    
+-- @B@,          2B@2          :B@B   ;@B@B@B@B@@@B@B;  B@Br     sB@B@@@B@B@B@B@B  @B@B@@@B@B@B@B#  :@B@B@B@B@B@@@B   B@B@B@B@B@B@B@B   2@B@B@B@B@B@@A  
+-- B@B:          2@BA          ,@@@                B@B  @B@r              iB@BB:               H@Br ;B@B        3B@B              9B@i ,@@@        B@B: 
+-- @B@,          2B@2          :B@B     r9@@B@B@B@h@B@  B@Br            s@B@#,      ,S#@B@B@@@h@B@s :@B@        ,@B@   ,5#B@B@@@B#B@B5 :B@B        ;25  
+-- @@B,          2@BA          :@B@    @@B@H##B##H9B@B  @@@;          2B@@#        2@B@#####BHGB@Bi :B@@        ;B@B  SB@BBH##B##ABB@s ,@B@             
+-- @@@,          2B@2          :B@B   A@B9         @B@  B@@;        2@B@3          @@@         5B@s :@B@        ;@BB  B@B         i@B3 ,B@B             
+-- B@B;          9@@h          ;@B@   ;B@B3isissii@B@H  sB@@2sS5  hB@B@2::rriiss;  B@B@iiisssi2B@B, ;B@B        rB@B  B@@Biisssii3@B@, ;@B@             
+-- hB@           sB@s          ,B@B    ,H@B@B@B@B@B@s    ;B@B@B@ rB@@@B@B@B@B@B@B   s@B@B@B@@@@@H,  ,@BH        :@BG   sB@@@B@B@B@Bh,  ,B@H             
+--
+
+
 
 require 'Eternal Prediction'
 
@@ -125,17 +210,15 @@ function Malzahar:LoadSpells()
 end
 
 function Malzahar:LoadMenu()
+
 	Tocsin = MenuElement({type = MENU, id = "Tocsin", name = "Tocsin Malzahar"})
-
-
-
 	Tocsin:MenuElement({name = "Malzahar", drop = {ScriptVersion}, leftIcon = "https://vignette2.wikia.nocookie.net/leagueoflegends/images/8/81/Malzahar_OriginalLoading.jpg"})
 	
 	--Combo
 
 	Tocsin:MenuElement({type = MENU, id = "Combo", name = "Combo Settings"})
 	Tocsin.Combo:MenuElement({id = "Q", name = "Use [Q]", value = true, leftIcon = Q.icon})
-	Tocsin.Combo:MenuElement({id = "W", name = "Use [W]", value = false, leftIcon = W.icon})
+	Tocsin.Combo:MenuElement({id = "W", name = "Use [W]", value = true, leftIcon = W.icon})
 	Tocsin.Combo:MenuElement({id = "E", name = "Use [E]", value = true, leftIcon = E.icon})
 	Tocsin.Combo:MenuElement({id = "R", name = "Use [R]", value = true, leftIcon = R.icon})
 	Tocsin.Combo:MenuElement({id = "Health", name = "Enemy Health % To Use R", value = 0.50, min = 0.1, max = 1, step = 0.05})
@@ -143,8 +226,8 @@ function Malzahar:LoadMenu()
 	--Harass
 
 	Tocsin:MenuElement({type = MENU, id = "Harass", name = "Harass Settings"})
-	Tocsin.Harass:MenuElement({id = "Q", name = "Use [Q]", value = false, leftIcon = Q.icon})
-	Tocsin.Harass:MenuElement({id = "W", name = "Use [W]", value = false, leftIcon = W.icon})
+	Tocsin.Harass:MenuElement({id = "Q", name = "Use [Q]", value = true, leftIcon = Q.icon})
+	Tocsin.Harass:MenuElement({id = "W", name = "Use [W]", value = true, leftIcon = W.icon})
 	Tocsin.Harass:MenuElement({id = "E", name = "Use [E]", value = true, leftIcon = E.icon})
 	Tocsin.Harass:MenuElement({id = "Mana", name = "Min Mana to Harass [%]", value = 0.35, min = 0.05, max = 1, step = 0.01})
 
@@ -156,7 +239,7 @@ function Malzahar:LoadMenu()
 	Tocsin.Clear:MenuElement({id = "E", name = "Use [E]", value = true, leftIcon = E.icon})
 	Tocsin.Clear:MenuElement({id = "Mana", name = "Min Mana to Clear [%]", value = 0.30, min = 0.05, max = 1, step = 0.01})
 
-		--Misc
+	--Misc
 
 	Tocsin:MenuElement({type = MENU, id = "Misc", name = "Misc Settings"})
 	Tocsin.Misc:MenuElement({id = "Qks", name = "Killsecure [Q]", value = true, leftIcon = Q.icon})
@@ -165,7 +248,7 @@ function Malzahar:LoadMenu()
 
 	--Eternal Prediction
 	Tocsin:MenuElement({type = MENU, id = "Pred", name = "Prediction Settings"})
-	Tocsin.Pred:MenuElement({id = "Chance", name = "Prediction Hitchance", value = 0.200, min = 0.05, max = 1, step = 0.025})
+	Tocsin.Pred:MenuElement({id = "Chance", name = "Prediction Hitchance Q", value = 0.200, min = 0.05, max = 1, step = 0.025})
 	
 	--Draw
 
@@ -193,7 +276,11 @@ function Malzahar:Combo()
 	local target = GetTarget(1000, "AD")
 	if not target then return end
 	if myHero.activeSpell.isChanneling == true then return end
-    if Tocsin.Combo.E:Value() and Ready(_E) and myHero.pos:DistanceTo(target.pos) < 630 then
+    if Tocsin.Combo.W:Value() and Ready(_W) and myHero.pos:DistanceTo(target.pos) < 650 then
+		self:CastW(target)
+	end
+
+	if Tocsin.Combo.E:Value() and Ready(_E) and myHero.pos:DistanceTo(target.pos) < 630 then
 		self:CastE(target)
 	end
 
@@ -205,9 +292,7 @@ function Malzahar:Combo()
 		self:CastR(target)
 	end
 
-	if Tocsin.Combo.W:Value() and Ready(_W) and myHero.pos:DistanceTo(target.pos) < 550 then
-		self:CastW(target)
-	end
+
 
 end
 
@@ -215,6 +300,11 @@ function Malzahar:Harass()
 	local target = GetTarget(1000)
 	if myHero.mana/myHero.maxMana < Tocsin.Harass.Mana:Value() then return end
 	if not target then return end
+
+	if Tocsin.Combo.W:Value() and Ready(_W) and myHero.pos:DistanceTo(target.pos) < 650 then
+		self:CastW(target)
+	end
+
 	if Tocsin.Combo.E:Value() and Ready(_E) and myHero.pos:DistanceTo(target.pos) < 630 then
 		self:CastE(target)
 	end
@@ -223,9 +313,7 @@ function Malzahar:Harass()
 		self:CastQ(target)
 	end
 
-	if Tocsin.Combo.W:Value() and Ready(_W) and myHero.pos:DistanceTo(target.pos) < 650 then
-		self:CastW(target)
-	end
+
 end
 
 function Malzahar:Clear()
@@ -236,17 +324,22 @@ function Malzahar:Clear()
 		    if  Tocsin.Clear.E:Value() and Ready(_E) and myHero.pos:DistanceTo(minion.pos) < 630 then
 				self:CastE(minion)
 			end
+
             if  Tocsin.Clear.Q:Value() and Ready(_Q) and myHero.pos:DistanceTo(minion.pos) < 870 then
 				self:CastQ(minion)
 			end
+
             if  Tocsin.Clear.W:Value() and Ready(_W) and myHero.pos:DistanceTo(minion.pos) < 650 then
 				self:CastW(minion)
 			end
+
 		end
 	end
 end
+
 --   80 / 115 / 150 / 185 / 220 (+ 80% AP) E
 --   70 / 105 / 140 / 175 / 210 (+ 80% AP) Q
+
 function Malzahar:Misc()
 	if myHero.activeSpell.isChanneling == true then return end
 	local target = GetTarget(850)
@@ -254,7 +347,7 @@ function Malzahar:Misc()
 		if Tocsin.Misc.Eks:Value() and Ready(_E) then
 			local lvl = myHero:GetSpellData(_E).level
 			local Edmg = (({80, 115, 150, 185, 220})[lvl] + .80 * myHero.ap)
-			if  Edmg > target.health + target.shieldAP then
+			if  Edmg > target.health * 1.2 + target.shieldAP then
 				self:CastE(target)
 			end
 		end
@@ -264,7 +357,7 @@ function Malzahar:Misc()
 		local Edmg = (({80, 115, 150, 185, 220})[Elvl] + .80 * myHero.ap)
         local Qdmg = (({70, 105, 140, 175, 210})[Elvl] + .80 * myHero.ap)
         local QEdmg = Edmg + Qdmg
-            if QEdmg > target.health * 1.2 + target.shieldAP then
+            if QEdmg > target.health * 1.3 + target.shieldAP then
                 self:CastE(target)
                 DelayAction(function() self:CastQ(target) end, 0.3)
             end
@@ -290,7 +383,7 @@ function Malzahar:CastQ(target)
 				    --if buff.name == MalzaharE then
 					    EnableOrb(false)
 			            Control.CastSpell(HK_Q, pred.castPos)
-			            DelayAction(function() EnableOrb(true) end, 0.3)
+			            DelayAction(function() EnableOrb(true) end, 0.4)
 				    --end 
 			    --end
 		    --end	
@@ -300,7 +393,7 @@ end
 
 function Malzahar:CastW(target)
 	if myHero.activeSpell.isChanneling == true then return end
-	if  myHero.pos:DistanceTo(target.pos) < 550 then
+	if  myHero.pos:DistanceTo(target.pos) < 650 then
 		Control.CastSpell(HK_W)
 	end
 end
@@ -354,7 +447,7 @@ function Malzahar:IsValidTarget(unit,range)
 end
 
 function Malzahar:Draw()
-	if Tocsin.Draw.Q:Value() and Ready(_Q) then Draw.Circle(myHero.pos, 1050, 3,  Draw.Color(255,255, 000, 000)) end
+	if Tocsin.Draw.Q:Value() and Ready(_Q) then Draw.Circle(myHero.pos, 870, 3,  Draw.Color(255,255, 000, 000)) end
 	if Tocsin.Draw.E:Value() and Ready(_E) then Draw.Circle(myHero.pos, 630, 3,  Draw.Color(255,255, 255, 100)) end
 end
 
@@ -364,6 +457,6 @@ Callback.Add("Load", function()
 	if _G[myHero.charName] then
 		_G[myHero.charName]()
 		print("Tocsin "..myHero.charName.." "..ScriptVersion.." Loaded")
-	else print ("Tocsin doens't support "..myHero.charName.." shutting down...") return
+	else print ("Tocsin doesn't support "..myHero.charName.." shutting down...") return
 	end
 end)
